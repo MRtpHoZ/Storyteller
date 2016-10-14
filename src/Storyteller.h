dd0c11c431ab174caa12253507e8293e3796d1a2
yatong.wu <yatong.wu@dena.jp>
2016-10-14 19:07:55 +0800

    quick upload

--- src/Storyteller.h
+++ src/Storyteller.h
@@ -4,5 +4,12 @@
 
 class Storyteller 
 {
+public:
+    Storyteller();
+    ~Storyteller();
 
+    // 
+    bool isGameEnded();
+    bool description(); // ask the Storyteller to describe the situation right now.
+    void choose(int choice); // input Player's choice on current situation.
 };
